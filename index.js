926390b216820c0c5f594d0a6edcff632ff86a34
aliprogrammer309 <aliprogrammer309@gmail.com>
2023-07-06 18:54:37 +0500

    added ejs functionality to the project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const { connectToMongoDB } = require("./connection");
 const URL = require("./models/url");
 const app = express();
@@ -8,25 +10,14 @@ const PORT = 8001;
 connectToMongoDB("mongodb://127.0.0.1:27017/url-shortener").then(() => {
   console.log("MongoDB connected!");
 });
-app.use(express.json());
-app.use("/url", urlRoute);
 
-// app.get("/:shortId", async (req, res) => {
-//   const shortId = req.params.shortId;
-//   const entry = await URL.findOneAndUpdate(
-//     {
-//       shortId,
-//     },
-//     {
-//       $push: {
-//         visitHistory: {
-//           timestamp: Date.now(),
-//         },
-//       },
-//     }
-//   );
-//   res.redirect(entry.redirectURL);
-// });
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/url", urlRoute);
+app.use("/", staticRoute);
+
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
